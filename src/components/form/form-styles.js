c8e9b7b6a98644c84aa75a3e38de0b2e65df29e3
Arnab Roy <trifiasco@gmail.com>
2020-07-24 03:14:48 +0600

    Add barebone styles file

---
+++ src/components/form/form-styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    formButton: {
+        position: 'absolute',
+        top: '30%',
+        left: '40%',
+    }
+}));
+
+export default useStyles;
