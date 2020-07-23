285256e7dc3209b1e81d9fc65d22725ae1f06040
Arnab Roy <trifiasco@gmail.com>
2020-07-23 23:32:58 +0600

    Add and integrate component for main serving point

---
+++ src/pages/main/Main-view.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../../components/navbar';
+import Form from '../../components/form';
+
+const Main = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <Form />
+        </div>
+    );
+
+};
+
+export default Main;
