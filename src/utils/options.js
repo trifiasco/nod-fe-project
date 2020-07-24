d5f7d05c6ab2863fc827caf7c20de4cafaf4af2a
Arnab Roy <trifiasco@gmail.com>
2020-07-24 10:16:47 +0600

    Add options  for select and multi-select lists

---
+++ src/utils/options.js
@@ -0,0 +1,14 @@
+const genders = [
+    "Female", "Male", "Other"
+];
+
+const recommendations = [
+    "Insurance",
+    "Superannuation",
+    "Investments",
+    "Cashflow",
+    "Pension",
+    "Estate Planning"
+];
+
+export {genders, recommendations};
